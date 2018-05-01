f02421e7bdb2a0ece285c659dd029e619b13be40
= <maxime.napoleon@gmail.com>
2018-05-01 13:02:04 +0200

    Lancement

---
+++ src/dominion/card/ActionCard.java
@@ -0,0 +1,12 @@
+package dominion.card;
+import java.util.*;
+import dominion.*;
+
+/**
+ * Les cartes Action
+ */
+public abstract class ActionCard extends Card {
+
+
+
+}
---
+++ src/dominion/card/VictoryCard.java
@@ -0,0 +1,24 @@
+package dominion.card;
+import java.util.*;
+import dominion.*;
+
+/**
+ * Les cartes Victoire
+ */
+public abstract class VictoryCard extends Card {
+
+
+	/**
+	 * Une carte Victoire renvoit des
+	 *
+	 * @param p joueur qui possède la carte (la valeur d'une carte peut dépendre du joueur qui la possède, c'est le cas des cartes Gardens)
+	 *
+	 * Toutes les cartes qui ne sont pas de type Victoire ont une valeur de 0
+	 * (la méthode devra donc être redéfinie pour les cartes ayant une valeur
+	 * @return
+	 */
+	@Override
+	public int victoryValue(Player p) {
+
+	}
+}
