6b08a740e3de4d994d4b9b590a754c2632de6bc3
= <maxime.napoleon@gmail.com>
2018-05-20 19:17:02 +0200

    Suppression de play et rajout de playerReact

--- src/dominion/card/AttackCard.java
+++ src/dominion/card/AttackCard.java
@@ -19,8 +19,18 @@ public abstract class AttackCard extends ActionCard {
     	return types;
 	}
 
-	@Override
-	public void play(Player p) {
-
+	/**
+	 * On check si le joueur peut et veut réagir
+	 * @param p
+	 * @return boolean {@code true si le joueur réagit}
+	 */
+	public boolean playerReact(Player p) {
+    	boolean flag = false; // valeur par défaut
+    	for (Card c: p.cardsInHand()) {
+    		if (c.getTypes().contains(CardType.Reaction) && flag == false) { // si la carte est une carte réaction
+    			flag = ((ReactionCard) c).reaction(p); // on cast la carte (qui est une reaction d'après le if) pour appliquer la méthode reaction retournant un boolean
+			} // si le boolean est vrai on stop et on return, sinon on attend (au cas ou il veut réagir avec une autre carte)
+		}
+		return flag;
 	}
 }
