b5995cbd977c602dec73d1a0f99218d264f101a7
= <maxime.napoleon@gmail.com>
2018-05-21 14:53:58 +0200

    Laboratoire fait

--- src/dominion/card/base/Laboratory.java
+++ src/dominion/card/base/Laboratory.java
@@ -10,4 +10,15 @@ import dominion.card.*;
  * +1 Action.
  */
 public class Laboratory extends ActionCard {
+
+	public Laboratory() {
+		super("Laboratory", 5);
+	}
+
+	@Override
+	public void play(Player p) {
+		p.addToHand(p.drawCard());
+		p.addToHand(p.drawCard());
+		p.incrementActions(1);
+	}
 }
