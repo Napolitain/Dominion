78cbefdb2bc7eeedfd5d03fbd7a6a0a81ff5477a
= <maxime.napoleon@gmail.com>
2018-05-21 16:48:18 +0200

    Scanner --> choose()

--- src/dominion/card/base/Chancellor.java
+++ src/dominion/card/base/Chancellor.java
@@ -19,7 +19,7 @@ public class Chancellor extends ActionCard {
 	public void play(Player p) {
 		p.incrementMoney(2);
 		List<String> choices = Arrays.asList("o", "n");
-		String input = choose("Voulez-vous défausser votre deck ? o/n", choices, false);
+		String input = p.choose("Voulez-vous défausser votre deck ? o/n", choices, false);
 		if (input.equals("o")) {
 			for (Card c: p.totalCards()) {
 				p.addToDiscard(c); // pas fini : addToDiscard remove une card de hand pas forcément de la ou elle est
