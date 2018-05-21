b6ea96839846e4115e04025de81b8a6698420a42
Antoine G <antog1999@gmail.com>
2018-05-21 15:08:15 +0000

    Replace Library.java

--- src/dominion/card/base/Library.java
+++ src/dominion/card/base/Library.java
@@ -3,6 +3,8 @@ import java.util.*;
 import dominion.*;
 import dominion.card.*;
 
+import javax.swing.*;
+
 /**
  * Carte Bibliothèque (Library)
  * 
@@ -10,29 +12,29 @@ import dominion.card.*;
  */
 public class Library extends ActionCard {
 
-	public Library() {
-		super("Library", 5);
-	}
+    public Library(){
+        super ("Library", 5);
+    }
 
-	@Override
-	public void play(Player p) {
-		/*CardList actionCards = new CardList();
-		Card c;
-		String input;
-		List<String> choices = Arrays.asList("o", "n");
-		while (p.cardsInHand().size() < 7) {
-			c = p.drawCard();
-			if (c == null) {
-				System.err.println("Il n'y a pas de carte à piocher (pioche et défausse vide).");
-				return;
-			}
-			p.addToHand(c);
-			if (c.getTypes().contains(CardType.Action)) {
-				actionCards.add(c);
-			}
-		}
-		for (c: actionCards) {
-			input = p.choose("Voulez vous défausser")
-		}*/
-	}
+    public void play(Player p){
+        CardList actionList = new CardList();
+        while (p.cardsInHand().size() < 7){
+            Card c = p.drawCard();
+            if (c == null) {
+                System.err.println("Il n'y a pas de carte à piocher (pioche et défausse vide).");
+                return;
+            }
+            p.addToHand(c);
+            if (c instanceof ActionCard) {
+                actionList.add(c);
+                String input = p.chooseCard("Voulez vous défausser cette carte ? o/n",actionList, false);
+                if (input == "o"){
+                    p.addToDiscard(c);
+                    p.cardsInHand().remove(c);
+                }
+            }
+
+        }
+
+    }
 }
