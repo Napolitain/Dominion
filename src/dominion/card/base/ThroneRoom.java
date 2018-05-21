3b265d7aab4f3fd295a5ebebb6243d26a2f7279f
= <maxime.napoleon@gmail.com>
2018-05-21 14:28:05 +0200

    Throne Room faite

--- src/dominion/card/base/ThroneRoom.java
+++ src/dominion/card/base/ThroneRoom.java
@@ -10,4 +10,15 @@ import dominion.card.*;
  * Jouez-la deux fois.
  */
 public class ThroneRoom extends ActionCard {
+
+	public ThroneRoom() {
+		super("Throne Room", 4);
+	}
+
+	@Override
+	public void play(Player p) {
+		String input = p.chooseCard("Choisissez une carte action de votre main à jouer deux fois.", p.getActionCards(), false);
+		p.getActionCards().getCard(input).play(p); // joue une carte sans défausser
+		p.playCard(input); // joue une carte en défaussant
+	}
 }
